c1415b29e222488113aafb6d5c8b86a1277376a5
Roy Yang <roy82859370@gmail.com>
2022-03-13 13:11:12 +0800

    'second_commit'

--- bmicount.py
+++ bmicount.py
@@ -1,5 +1,15 @@
-cm=int(input('請輸入你的身高:'))
-kg=int(input('請輸入你的體重:'))
+cm=float(input('請輸入你的身高:'))
+kg=float(input('請輸入你的體重:'))
 tall=cm/100
 bmi=round(kg/(tall**2),2)
 print('你的bmi:',bmi)
+if bmi<18.5 :
+	print('體重過輕')
+elif bmi>=18.5 and bmi<24:
+	print('體重正常')
+elif bmi>=27 and bmi<30:
+	print('輕度肥胖')
+elif bmi>=30 and bmi<35:
+	print('中度肥胖')
+else:
+    print('重度肥胖')
